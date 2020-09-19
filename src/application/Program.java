564ad442c231b79a2cc2960b89444081e9746c60
Alexandre Pereira <alexandresp15@gmail.com>
2020-09-19 20:39:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Book");
+		Locale.setDefault(Locale.US);
 		
+		Department department = new Department(1, "Book");
+
 		System.out.println(department);
+		
+		Seller seller = new Seller(21, "Paulo", "paulo@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
+		
 
 	}
 
