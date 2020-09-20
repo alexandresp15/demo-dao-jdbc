00014921ea0a1493b5a563de26b1776549957cef
Alexandre Pereira <alexandresp15@gmail.com>
2020-09-19 21:30:50 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,8 @@ public class Program {
 		System.out.println(department);
 		
 		Seller seller = new Seller(21, "Paulo", "paulo@gmail.com", new Date(), 3000.0, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
